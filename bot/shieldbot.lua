fa57f32ed849f1a2194496d336e9bca894236014
javyy <javyy64@gmail.com>
2016-10-18 03:16:35 -0700

    Update shieldbot.lua

--- bot/shieldbot.lua
+++ bot/shieldbot.lua
@@ -251,7 +251,7 @@ function create_config( )
     "sh",
     "serverinfo"
     },
-    sudo_users = {80182995},--Sudo users
+    sudo_users = {278132257},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[MeGa shield v2.9
 An advanced administration bot based on TG-CLI written in Lua
@@ -726,7 +726,7 @@ function cron_plugins()
 end
 
 -- Start and load values
-our_id = 0
+our_id = 278132257
 now = os.time()
 math.randomseed(now)
 started = false
